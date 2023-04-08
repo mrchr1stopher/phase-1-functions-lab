c14aae2f12a5fbd515e6b0c062b3700d0a8bd506
Christopher Wiggins <cwiggins610@gmail.com>
2023-04-07 20:33:54 -0400

    test passing

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const hqStreetNumber = 42;
+const blocksInFeet = 264;
+
+function distanceFromHqInBlocks(streetNumber) {
+    const blocks = Math.abs(streetNumber - hqStreetNumber);
+    return blocks;
+  }
+
+  function distanceFromHqInFeet(streetNumber) {
+    const distanceInBlocks = distanceFromHqInBlocks(streetNumber);
+    const distanceInFeet = distanceInBlocks * blocksInFeet;
+    return distanceInFeet;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(destination - start);
+    const distanceInFeet = blocks * blocksInFeet;
+    return distanceInFeet;
+  }
+
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    let farePrice;
+  
+    if (distanceInFeet <= 400) {
+      farePrice = 0;
+    } else if (distanceInFeet <= 2000) {
+      farePrice = (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet <= 2500) {
+      farePrice = 25;
+    } else {
+      farePrice = 'cannot travel that far';
+    }
+  
+    return farePrice;
+  }
